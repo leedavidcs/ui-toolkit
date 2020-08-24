5a35eabcfaa6d428f613dee9c43d105456881f40
leedavidcs <lee.david.cs@gmail.com>
2020-08-24 16:57:10 -0700

    Add tracker for fixed-cells

--- src/components/table.component/row.component/index.tsx
+++ src/components/table.component/row.component/index.tsx
@@ -1,8 +1,18 @@
+import type { ICellProps } from "@/components/table.component/cell.component";
 import { ITableContext, TableContext } from "@/components/table.component/context";
 import clsx from "clsx";
-import React, { cloneElement, CSSProperties, FC, memo, useContext, useMemo } from "react";
+import React, {
+	Children,
+	cloneElement,
+	CSSProperties,
+	FC,
+	memo,
+	ReactElement,
+	useContext,
+	useMemo
+} from "react";
 import { SortableElement } from "react-sortable-hoc";
-import { ListChildComponentProps } from "react-window";
+import type { ListChildComponentProps } from "react-window";
 import classes from "./styles.module.scss";
 
 export const DEFAULT_ROW_HEIGHT = 40;
@@ -15,8 +25,8 @@ interface ISortableRowProps<T extends unknown = any> {
 
 const memoSortable = <P extends unknown>(element: FC<P>) => memo(SortableElement(element));
 
-const SortableRow = memoSortable(({ data, rowIndex, style }: ISortableRowProps) => {
-	const { bodyCells, sortable } = useContext<ITableContext>(TableContext);
+const useRowCells = ({ data, rowIndex }: ISortableRowProps) => {
+	const { bodyCells } = useContext<ITableContext>(TableContext);
 
 	const rowData = data[rowIndex];
 
@@ -29,17 +39,63 @@ const SortableRow = memoSortable(({ data, rowIndex, style }: ISortableRowProps) 
 		});
 	}, [bodyCells, rowData]);
 
+	return useMemo(() => {
+		const leftFixedCells: ReactElement<ICellProps>[] = [];
+		const rightFixedCells: ReactElement<ICellProps>[] = [];
+		const scrollableCells: ReactElement<ICellProps>[] = [];
+
+		let leftGroupWidth: number = 0;
+		let rightGroupWidth: number = 0;
+
+		Children.forEach(rowCells, (cell, i) => {
+			const { fixed, width = 0 } = cell.props;
+
+			switch (fixed) {
+				case "left": {
+					leftFixedCells.push(cell);
+					leftGroupWidth += width;
+
+					break;
+				}
+				case "right": {
+					rightFixedCells.push(cell);
+					rightGroupWidth += width;
+
+					break;
+				}
+				default: {
+					scrollableCells.push(cell);
+				}
+			}
+		});
+
+		const cells = [leftFixedCells, scrollableCells, rightFixedCells] as readonly [
+			readonly ReactElement<ICellProps>[],
+			readonly ReactElement<ICellProps>[],
+			readonly ReactElement<ICellProps>[]
+		];
+
+		const widths = [leftGroupWidth, rightGroupWidth] as readonly [number, number];
+
+		return { cells, widths };
+	}, [rowCells]);
+};
+
+const SortableRow = memoSortable((props: ISortableRowProps) => {
+	const { style } = props;
+
+	const { sortable } = useContext<ITableContext>(TableContext);
+
+	const {
+		cells: [, middleCells]
+	} = useRowCells(props);
+
 	return (
-		<div className={clsx(classes.root, "uitk-table-row")} style={style}>
-			<div
-				className={clsx(
-					classes.cellContainer,
-					{ [classes.sortable]: sortable },
-					"uitk-table-row-cell-container"
-				)}
-			>
-				{rowCells}
-			</div>
+		<div
+			className={clsx(classes.root, { [classes.sortable]: sortable }, "uitk-table-row")}
+			style={style}
+		>
+			{middleCells}
 		</div>
 	);
 });
