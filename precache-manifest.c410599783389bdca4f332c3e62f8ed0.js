e736f79c220a33cc04b14e44ed873ad9a14fa193
leedavidcs <lee.david.cs@gmail.com>
2020-08-24 17:01:47 -0700

    Updates

---
+++ precache-manifest.c410599783389bdca4f332c3e62f8ed0.js
@@ -0,0 +1,29 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "url": "0.083f37ec83137e47e8c0.bundle.js"
+  },
+  {
+    "revision": "a8c8e9d2380b5ccb748e235f5f13648d",
+    "url": "iframe.html"
+  },
+  {
+    "url": "main.083f37ec83137e47e8c0.bundle.js"
+  },
+  {
+    "url": "runtime~main.083f37ec83137e47e8c0.bundle.js"
+  },
+  {
+    "revision": "b63bac956e464a29f10f",
+    "url": "static/css/main.54f79df3.chunk.css"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "url": "vendors~main.083f37ec83137e47e8c0.bundle.js"
+  },
+  {
+    "url": "vendors~main.083f37ec83137e47e8c0.bundle.js.LICENSE.txt"
+  }
+]);
